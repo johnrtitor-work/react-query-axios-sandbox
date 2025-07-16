e497b5672246ce16c191244dd493d55c8ad1e970
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-07-16 20:03:47 +0530

    setup tanstack query
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+
+// create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <HelloWorld />
-    </>
+    </QueryClientProvider>
   );
 }
 
