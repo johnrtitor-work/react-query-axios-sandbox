efe282be1a639b5a180ddb605a1d9e266b6276a8
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-07-18 18:53:14 +0530

    demonstrate using mutation
    mutation in tanstack query is a way to update the data/submit the data
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/components/DemoTodo.tsx
+++ src/components/DemoTodo.tsx
@@ -1,15 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTodos, type Todo } from "../api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addTodo, fetchTodos, type Todo } from "../api";
 import TodoCard from "./TodoCard";
+import { useState } from "react";
 
 export default function DemoTodo() {
+  const [title, setTitle] = useState("");
+
   const {
     isPending,
     error,
     data: todos,
+    refetch: refetchTodo,
   } = useQuery<Todo[]>({
     queryKey: ["fetchtodo"],
     queryFn: () => fetchTodos(),
+  });
+
+  const { mutateAsync: addTodoMutation } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      refetchTodo();
+    },
   });
 
   if (isPending) {
@@ -23,6 +34,33 @@ export default function DemoTodo() {
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Need to code!"
+          // e.target.value is the current text in the box
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <button
+          type="button"
+          onClick={async () => {
+            try {
+              // send a request to the API to add the todo
+              await addTodoMutation({
+                title,
+              });
+              // reset the title field of add todo text box
+              setTitle("");
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+        >
+          Add task
+        </button>
+      </div>
       <h2>List of tasks: </h2>
       {todos?.map((todo) => {
         return (
